e3a2fd0c4a883a3c85631bb68b8d1f20e551ab55
max7im-prog <nogagorilli@gmail.com>
2025-06-10 12:41:54 +0300

    Implemented polling system for key and button presses

--- src/events/pollEventSystem.cpp
+++ src/events/pollEventSystem.cpp
@@ -4,6 +4,8 @@
 
 #include "events.hpp"
 
+#include <iostream>
+
 PollEventSystem::PollEventSystem()
 {
 }
@@ -14,11 +16,38 @@ PollEventSystem::~PollEventSystem()
 
 void PollEventSystem::update(entt::registry &registry)
 {
+    // Remove all player input from previous iteration
+    auto view = registry.view<PlayerInput>();
+    registry.destroy(view.begin(), view.end());
+
+
+    // Handle player input
     SDL_Event event;
-    while(SDL_PollEvent(&event)){
-        if(event.type == SDL_EVENT_QUIT){
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_EVENT_QUIT)
+        {
             auto ent = registry.create();
             registry.emplace<QuitEvent>(ent);
+            registry.emplace<PlayerInput>(ent);
+        }
+        else if (event.type == SDL_EVENT_KEY_DOWN || event.type == SDL_EVENT_KEY_UP)
+        {
+            auto ent = registry.create();
+            registry.emplace<PlayerInput>(ent);
+
+            auto &kp = registry.emplace<KeyPress>(ent);
+            kp.key = event.key.key; // SDL_Keycode
+            kp.pressed = (event.type == SDL_EVENT_KEY_DOWN);
+        }
+        else if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN || event.type == SDL_EVENT_MOUSE_BUTTON_UP)
+        {
+            auto ent = registry.create();
+            registry.emplace<PlayerInput>(ent);
+
+            auto &bp = registry.emplace<ButtonPress>(ent);
+            bp.button = event.button.button; // SDL button code
+            bp.pressed = (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN);
         }
     }
 }
