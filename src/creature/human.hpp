8856b3fcc4ff3ccf0544ca343f9a78511b2c2100
max7im-prog <nogagorilli@gmail.com>
2025-08-03 21:12:06 +0300

    I am going to fullly redesign this

--- src/creature/human.hpp
+++ src/creature/human.hpp
@@ -5,31 +5,33 @@
 #include "circleBodyPart.hpp"
 #include "limbBodyPart.hpp"
 #include <memory>
-class Human: public BaseCreature{
+class Human : public BaseCreature {
 public:
-    struct HumanConfig{
+  struct HumanConfig {};
 
-    };
-
-    std::shared_ptr<Human> create(HumanConfig config);
-    static std::optional<HumanConfig> parseConfigFromJSON(const std::string &fileName);
-
+  std::shared_ptr<Human> create(HumanConfig config);
+  static std::optional<HumanConfig>
+  parseConfigFromJSON(const std::string &fileName);
 
 protected:
-    std::shared_ptr<CapsuleBodyPart> torso;
-    std::shared_ptr<CapsuleBodyPart> neck;
-    std::shared_ptr<CircleBodyPart> leftShoulder;
-    std::shared_ptr<CircleBodyPart> rightShoulder;
-    std::shared_ptr<CircleBodyPart> leftHip;
-    std::shared_ptr<CircleBodyPart> rightHip;
+  Human();
+  std::shared_ptr<CapsuleBodyPart> torso;
+  std::shared_ptr<CapsuleBodyPart> neck;
+  std::shared_ptr<CircleBodyPart> leftShoulder;
+  std::shared_ptr<CircleBodyPart> rightShoulder;
+  std::shared_ptr<CircleBodyPart> leftHip;
+  std::shared_ptr<CircleBodyPart> rightHip;
 
-    std::shared_ptr<LimbBodyPart> leftLeg;
-    std::shared_ptr<LimbBodyPart> rightLeg;
-    std::shared_ptr<LimbBodyPart> leftArm;
-    std::shared_ptr<LimbBodyPart> rightArm;
+  std::shared_ptr<LimbBodyPart> leftLeg;
+  std::shared_ptr<LimbBodyPart> rightLeg;
+  std::shared_ptr<LimbBodyPart> leftArm;
+  std::shared_ptr<LimbBodyPart> rightArm;
 
-    PIDScalarController torsoRotationController;
-    PIDScalarController torsoHeightController;
-    
+  PIDScalarController torsoRotationController;
+  PIDScalarController torsoHeightController;
 
+  b2JointId leftHipPrismaticJoint;
+  b2JointId rightHipPrismaticJoint;
+  b2JointId leftArmPrismaticJoint;
+  b2JointId rightArmPrismaticJoint;
 };
