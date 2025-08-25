54dfb011c9dc95b519549ebca458a1fd91431d2d
max7im <nogagorilli@gmail.com>
2025-08-25 17:44:27 +0300

    Defined the structure  of a template functions in factory, i guess.

--- src/interfaces/registryObjectFactory.cpp
+++ src/interfaces/registryObjectFactory.cpp
@@ -4,3 +4,12 @@ RegistryObjectFactory::RegistryObjectFactory(entt::registry &registry)
     : registry(registry) {}
 
 RegistryObjectFactory::~RegistryObjectFactory() {}
+
+template <typename T>
+std::shared_ptr<T> RegistryObjectFactory::create(
+    const typename FacturyConfigMapping<T>::Config &config) {
+  return nullptr;
+}
+
+template <typename T>
+void RegistryObjectFactory::registerObject(const std::shared_ptr<T> object) {}
--- src/interfaces/registryObjectFactory.hpp
+++ src/interfaces/registryObjectFactory.hpp
@@ -1,12 +1,22 @@
 #pragma once
 #include <entt/entt.hpp>
+
+template <typename T> struct FacturyConfigMapping;
+
 class RegistryObjectFactory {
 public:
   virtual ~RegistryObjectFactory() = 0;
 
+  template <typename T>
+  std::shared_ptr<T>
+  create(const typename FacturyConfigMapping<T>::Config &config);
+
 protected:
   RegistryObjectFactory(entt::registry &registry);
   entt::registry &registry;
+
+  template<typename T>
+  void registerObject(const std::shared_ptr<T> object);
 
 private:
   RegistryObjectFactory() = delete;
