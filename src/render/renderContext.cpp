6c76be3ad3b46f7f31fd31682fe39678f651fe46
max7im <nogagorilli@gmail.com>
2025-10-05 12:18:18 +0300

    Reimplemented renderContext??????

---
+++ src/render/renderContext.cpp
@@ -0,0 +1,64 @@
+#include "renderContext.hpp"
+#include <stdexcept>
+
+RenderContext::RenderContext(const RenderContextConfig &cfg) {
+  window = SDL_CreateWindow(cfg.WindowTitle.c_str(), cfg.widthPixels,
+                            cfg.heightPixels, 0);
+  if (!window) {
+    throw std::runtime_error("Failed to create SDL window");
+  }
+
+  renderer = SDL_CreateRenderer(window, nullptr);
+  if (!renderer) {
+    SDL_DestroyWindow(window);
+    throw std::runtime_error("Failed to create SDL Renderer");
+  }
+
+  pixelToMeterRatio = cfg.pixelToMeterRatio;
+  basePos = cfg.basePos;
+}
+
+RenderContext::~RenderContext() {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+}
+
+RenderContext
+RenderContext::createNewRenderContext(const RenderContextConfig &cfg) {
+  RenderContext ret(cfg);
+  return ret;
+}
+
+SDL_Renderer *RenderContext::getSDLRenderer() { return renderer; }
+
+SDL_Window *RenderContext::getSDLWindow() { return window; }
+
+int RenderContext::getWidthPixels() {
+  int w, h;
+  SDL_GetWindowSizeInPixels(window, &w, &h);
+  return w;
+}
+
+int RenderContext::getHeightPixels() {
+  int w, h;
+  SDL_GetWindowSizeInPixels(window, &w, &h);
+  return h;
+}
+
+float RenderContext::getWidthMeters() {
+  return (float(getWidthPixels())) / pixelToMeterRatio;
+}
+
+float RenderContext::getHeightMeters() {
+  return (float(getHeightPixels())) / pixelToMeterRatio;
+}
+
+float RenderContext::getPixelToMeterRatio() { return pixelToMeterRatio; }
+
+void RenderContext::setPixelToMeterRatio(float ratio) {
+  pixelToMeterRatio = ratio;
+}
+
+b2Vec2 RenderContext::getBasePos() { return basePos; }
+
+void RenderContext::setBasePos(b2Vec2 p) { basePos = p; }
--- src/render/renderContext.hpp
+++ src/render/renderContext.hpp
@@ -1,18 +1,67 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <box2d/box2d.h>
+#include <string>
 
-class RenderContext
-{
+struct RenderContextConfig {
+  static RenderContextConfig defaultConfig();
+
+  float pixelToMeterRatio = 100;
+  int widthPixels;
+  int heightPixels;
+  b2Vec2 basePos;
+
+  std::string WindowTitle;
+};
+
+class RenderContext {
 public:
-    SDL_Renderer *renderer;
-    SDL_Window *window;
+  [[nodiscard]] static RenderContext
+  createNewRenderContext(const RenderContextConfig &cfg);
 
-    // Position of bottom left point of a window in box2d meters
-    float x,y;
+  ~RenderContext();
 
-    // Width and height of a window in pixels
-    float w,h;
+  SDL_Renderer *getSDLRenderer();
+  SDL_Window *getSDLWindow();
 
-    // Conversion from box2d meters to pixels
-    float pixelToMeterRatio;
+  int getWidthPixels();
+  int getHeightPixels();
+
+  float getWidthMeters();
+  float getHeightMeters();
+
+  float getPixelToMeterRatio();
+  void setPixelToMeterRatio(float ratio);
+
+  /**
+   * @brief Get the position of a top left corner of the screen in box2d world
+   */
+  b2Vec2 getBasePos();
+
+  /**
+   * @brief Set the position of a top left corner of the screen in box2d world
+   */
+  void setBasePos(b2Vec2 p);
+
+private:
+  RenderContext(const RenderContextConfig &cfg);
+
+  /**
+   * @brief Conversion from box2d meters to pixels
+   */
+  float pixelToMeterRatio;
+
+  /**
+   * @brief Position of a top left corner of the screen in box2d world
+   */
+  b2Vec2 basePos;
+
+  SDL_Renderer *renderer;
+  SDL_Window *window;
+
+  RenderContext() = delete;
+  RenderContext(const RenderContext &) = delete;
+  RenderContext &operator=(const RenderContext &) = delete;
+  RenderContext(RenderContext &&) = default;
+  RenderContext &operator=(RenderContext &&) = default;
 };
