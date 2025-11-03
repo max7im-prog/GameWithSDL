63b18efd0c2dff677afb5ea86b557e767fac9f2f
max7im <nogagorilli@gmail.com>
2025-11-03 08:51:09 +0300

    Implemented a draft of a dispatch function for a polgonTerrain

--- src/room/roomManager.cpp
+++ src/room/roomManager.cpp
@@ -1,6 +1,55 @@
 #include "roomManager.hpp"
+#include "jsonUtils.hpp"
+#include "polygonTerrain.hpp"
+#include "roomIdentifiers.hpp"
 #include "roomProxy.hpp"
 #include <optional>
+
+RoomManager::RoomManager(std::shared_ptr<World> world,
+                         std::shared_ptr<CreatureFactory> creatureFactory,
+                         std::shared_ptr<TerrainFactory> terrainFactory)
+    : _world(world), _creatureFactory(creatureFactory),
+      _terrainFactory(terrainFactory), _entities(), _rooms() {}
+
+namespace {
+EntityId createEntityId(const nlohmann::json &roomJson,
+                        const nlohmann::json &entityConfigJson) {
+  EntityId ret = "";
+  ret += roomJson.at("id");
+  ret += "/";
+  ret += entityConfigJson.at("id");
+  return ret;
+}
+} // namespace
+
+const std::map<std::string,
+               std::function<void(const RoomManager::EntityDispatchContext &)>>
+    RoomManager::entityDispatchTable = {
+        {"PolygonTerrain",
+         [](const RoomManager::EntityDispatchContext &context) {
+           std::string entityId =
+               createEntityId(context._room->getJSON(), context._configJson);
+           if (context._mgr._entities.contains(entityId)) {
+             return;
+           }
+           PolygonTerrain::Config cfg;
+           cfg.defaultConfig();
+           cfg.fromJSON(context._configJson);
+           float x = context._room->getJSON().at("x").get<float>() +
+                     context._metadataJson.at("x").get<float>();
+           float y = context._room->getJSON().at("y").get<float>() +
+                     context._metadataJson.at("y").get<float>();
+
+           b2Rot rot = b2MakeRot(
+               (B2_PI * 2) * context._metadataJson.at("rotation").get<float>() /
+               360.0f);
+
+           auto factory = context._mgr._terrainFactory;
+           auto ent = factory->create<PolygonTerrain>(cfg);
+           context._mgr._entities.insert({entityId, ent});
+         }}
+
+};
 
 std::optional<RoomId> RoomManager::preloadRoom(std::string_view roomFile) {
   std::shared_ptr<RoomProxy> room = std::make_shared<RoomProxy>();
@@ -38,4 +87,31 @@ void RoomManager::unloadEntity(const EntityId &entityId) {
   auto entityPtr = it->second;
   _world->removeObject(entityPtr);
   _entities.erase(it);
+}
+
+std::optional<RoomId> RoomManager::loadRoom(const RoomId &roomId) {
+  auto it = _rooms.find(roomId);
+  if (it == _rooms.end()) {
+    return std::nullopt;
+  }
+  auto room = it->second;
+  auto roomJson = room->getJSON();
+  for (auto &entityMetadataJson : roomJson["entities"]) {
+    auto configFile = entityMetadataJson.at("configFile").get<std::string>();
+    auto temp = JsonUtils::parseJSON(configFile);
+    if (!temp) {
+      // TODO: log error
+      continue;
+    }
+    auto entityConfigJson = *temp;
+    auto entityType = entityConfigJson.at("type").get<std::string>();
+
+    if (RoomManager::entityDispatchTable.contains(entityType)) {
+      RoomManager::EntityDispatchContext context = {
+          *this, room, entityMetadataJson, entityConfigJson};
+      RoomManager::entityDispatchTable.at(std::string(entityType))(context);
+    } else {
+      // TODO: log error
+    }
+  }
 }
--- src/room/roomManager.hpp
+++ src/room/roomManager.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
+#include "creatureFactory.hpp"
 #include "registryComposite.hpp"
 #include "roomIdentifiers.hpp"
 #include "roomProxy.hpp"
+#include "terrainFactory.hpp"
 #include "world.hpp"
 
 class RoomManager {
 public:
+  RoomManager(std::shared_ptr<World> world,
+              std::shared_ptr<CreatureFactory> creatureFactory,
+              std::shared_ptr<TerrainFactory> terrainFactory);
   /**
-   * @brief Creates a RoomProxy for a selected roomFile. Has to be run before
-   * loadRoom() for system to work
+   * @brief Creates a RoomProxy for a selected roomFile. Has to be run
+   * before loadRoom() for system to work
    *
    * @param roomFile
    *
@@ -21,9 +26,10 @@ public:
    * @brief Loads all the entities in a room into world except for ones that are
    * already loaded.
    *
-   * @param roomId
+   * @param std::optional<RoomId> Returns the RoomId of a loaded room. Returns
+   * nullopt on failure
    */
-  void loadRoom(const RoomId &roomId);
+  std::optional<RoomId> loadRoom(const RoomId &roomId);
 
   const std::map<RoomId, std::shared_ptr<RoomProxy>> &getRooms();
   const std::map<EntityId, std::weak_ptr<RegistryComposite>> &getEntities();
@@ -33,7 +39,20 @@ public:
 
 protected:
 private:
+  struct EntityDispatchContext {
+    RoomManager &_mgr;
+    std::shared_ptr<RoomProxy> _room;
+    const nlohmann::json &_metadataJson;
+    const nlohmann::json &_configJson;
+  };
+
+  static const std::map<std::string,
+                        std::function<void(const EntityDispatchContext &)>>
+      entityDispatchTable;
+
   std::map<RoomId, std::shared_ptr<RoomProxy>> _rooms;
   std::map<EntityId, std::weak_ptr<RegistryComposite>> _entities;
-  std::shared_ptr<World> _world;
+  const std::shared_ptr<World> _world;
+  const std::shared_ptr<CreatureFactory> _creatureFactory;
+  const std::shared_ptr<TerrainFactory> _terrainFactory;
 };
