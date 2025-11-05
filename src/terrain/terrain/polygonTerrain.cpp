6269e9a579012ffa70cb9f7c25d2743afaf19595
max7im <nogagorilli@gmail.com>
2025-11-05 14:33:43 +0300

    Implemented scaling and transforms for PolygonTerrain

--- src/terrain/terrain/polygonTerrain.cpp
+++ src/terrain/terrain/polygonTerrain.cpp
@@ -1,5 +1,6 @@
 #include "polygonTerrain.hpp"
 #include "box2d/types.h"
+#include "jsonUtils.hpp"
 #include "polygonBody.hpp"
 #include "terrain.hpp"
 
@@ -11,11 +12,22 @@ PolygonTerrain::PolygonTerrain(
     : Terrain(registry, world) {
 
   // A single polygon
-  auto terrainPos = b2Add(config._transform._originPos, config._transform._relativePos);
-  auto terrainRot = b2MulRot(config._transform._rootRot, config._transform._relativeRot);
+  auto terrainPos =
+      b2Add(config._transform._originPos, config._transform._relativePos);
+  auto terrainRot =
+      b2MulRot(config._transform._rootRot, config._transform._relativeRot);
+
+  auto transformedVertices = config.vertices;
+  std::for_each(transformedVertices.begin(), transformedVertices.end(),
+                [&config](b2Vec2 &v) {
+                  v.x = config._transform._scaleX * v.x *
+                        (config._transform._flipX ? -1 : 1);
+                  v.y = config._transform._scaleY * v.y *
+                        (config._transform._flipY ? -1 : 1);
+                });
   {
     auto bodyCfg = config.templateBodyCfg;
-    bodyCfg.shapeCfg.vertices = config.vertices;
+    bodyCfg.shapeCfg.vertices = transformedVertices;
     bodyCfg.shapeCfg.bodyDef.position = terrainPos;
     bodyCfg.shapeCfg.bodyDef.rotation = terrainRot;
     bodyCfg.shapeCfg.bodyDef.type = b2_staticBody;
@@ -36,10 +48,28 @@ void PolygonTerrainConfig::fromJSON(const nlohmann::json &json) {
   defaultConfig();
   auto vtcs = json.at("vertices");
   vertices = {};
-  for(auto& v: vtcs ){
-    float x = v.at("x").get<float>();
-    float y = v.at("y").get<float>();
-    vertices.push_back({x,y});
+
+  // Read vertices
+  for (auto &v : vtcs) {
+    float x{0};
+    float y{0};
+    {
+      auto temp = JsonUtils::getOptional<float>(v, "x");
+      if (!temp) {
+        // TODO: log error
+        continue;
+      }
+      x = *temp;
+    }
+    {
+      auto temp = JsonUtils::getOptional<float>(v, "y");
+      if (!temp) {
+        // TODO: log error
+        continue;
+      }
+      y = *temp;
+    }
+    vertices.push_back({x, y});
   }
   // TODO: Complete
 }
