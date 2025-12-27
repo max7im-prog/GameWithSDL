ef2fcc42b1cd2f24944b62d5bddbdf8963b54393
max7im <nogagorilli@gmail.com>
2025-12-27 14:34:13 +0300

    Changed the creature interface

--- src/sceneNode/creature/creature/creature.hpp
+++ src/sceneNode/creature/creature/creature.hpp
@@ -9,33 +9,33 @@ struct CreatureConfig : SceneNodeConfig {
   static b2Filter defaultFilter();
 };
 
+enum class CreatureAction {
+  PrimaryMobility,
+  SecondaryMobility,
+  PrimaryAttack,
+  SecondaryAttack,
+  Defensive,
+  Interact,
+  CreatureActionCount
+};
 
 class Creature : public SceneNode {
 public:
   virtual ~Creature() = 0;
-
-  virtual void move(b2Vec2 dir, float intensity) = 0;
-  virtual void jump() {};
-  virtual void attack() {};
-  virtual void defend() {};
-  virtual void aim([[maybe_unused]] b2Vec2 worldPoint,
-                   [[maybe_unused]] bool aim) {};
-  virtual void lookAt([[maybe_unused]] b2Vec2 worldPoint,
+  virtual void move(const b2Vec2 &dir) = 0;
+  virtual void lookAt([[maybe_unused]] const b2Vec2 &worldPoint,
                       [[maybe_unused]] bool look) {};
-
+  virtual void perform(CreatureAction action, bool pressed);
   virtual void update(float dt) override;
-
-  std::uint32_t getAbilities();
 
 protected:
   Creature(entt::registry &registry, const std::shared_ptr<World> world);
 
-  const std::shared_ptr<World> world;
-
   void registerBody(std::weak_ptr<Body> body, const std::string &name);
-
   void registerConnection(std::weak_ptr<Connection> connection,
                           const std::string &name);
+
+  const std::shared_ptr<World> world;
 
 private:
   Creature() = delete;
