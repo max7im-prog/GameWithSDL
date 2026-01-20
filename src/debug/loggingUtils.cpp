86aaeeadc25da74e6fc2a11a45825f1fba4e3d73
max7im <nogagorilli@gmail.com>
2026-01-20 16:29:58 +0300

    Updated log flush policy to flush on errors

--- src/debug/loggingUtils.cpp
+++ src/debug/loggingUtils.cpp
@@ -13,6 +13,7 @@ bool LoggingUtils::initLogger(const std::string &loggerPath) {
         spdlog::rotating_logger_mt("baseLogger", loggerPath, maxSize, maxFiles);
     spdlog::set_default_logger(logger);
     spdlog::flush_every(std::chrono::seconds(1));
+    spdlog::flush_on(spdlog::level::err);
   } catch (const spdlog::spdlog_ex &ex) {
     std::cout << "Log init falied: " << ex.what() << std::endl;
     return false;
